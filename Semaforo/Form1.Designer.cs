bc0cdcb7835765f03f5eed1f745a08d4bb0e6ced
Sérgio Santos <tavinhomelo69@gmail.com>
2025-05-30 14:04:45 -0300

    Projeto Finalizado

---
+++ Semaforo/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Semaforo
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            openFileDialog1 = new OpenFileDialog();
+            pibSemaforo = new PictureBox();
+            btnIniciar = new Button();
+            btnParar = new Button();
+            timer1 = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)pibSemaforo).BeginInit();
+            SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pibSemaforo
+            // 
+            pibSemaforo.Image = Properties.Resources.Apagado;
+            pibSemaforo.Location = new Point(3, 1);
+            pibSemaforo.Name = "pibSemaforo";
+            pibSemaforo.Size = new Size(211, 329);
+            pibSemaforo.SizeMode = PictureBoxSizeMode.StretchImage;
+            pibSemaforo.TabIndex = 0;
+            pibSemaforo.TabStop = false;
+            // 
+            // btnIniciar
+            // 
+            btnIniciar.Location = new Point(3, 336);
+            btnIniciar.Name = "btnIniciar";
+            btnIniciar.Size = new Size(211, 39);
+            btnIniciar.TabIndex = 1;
+            btnIniciar.Text = "iniciar";
+            btnIniciar.UseVisualStyleBackColor = true;
+            btnIniciar.Click += btnIniciar_Click;
+            // 
+            // btnParar
+            // 
+            btnParar.Location = new Point(3, 381);
+            btnParar.Name = "btnParar";
+            btnParar.Size = new Size(211, 39);
+            btnParar.TabIndex = 2;
+            btnParar.Text = "Parar";
+            btnParar.UseVisualStyleBackColor = true;
+            btnParar.Click += btnParar_Click;
+            // 
+            // timer1
+            // 
+            timer1.Interval = 1000;
+            timer1.Tick += timer1_Tick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(218, 428);
+            Controls.Add(btnParar);
+            Controls.Add(btnIniciar);
+            Controls.Add(pibSemaforo);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pibSemaforo).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private OpenFileDialog openFileDialog1;
+        private PictureBox pibSemaforo;
+        private Button btnIniciar;
+        private Button btnParar;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ Semaforo/Form1.cs
@@ -0,0 +1,51 @@
+using Semaforo.Properties;
+
+namespace Semaforo
+{
+    public partial class Form1 : Form
+    {
+        private int estadoAtual = 1;
+        private bool semafaroAtivo;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            semafaroAtivo = true;
+            estadoAtual = 1;
+            timer1.Start();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Muda o estado e atualiza a imagem
+            switch (estadoAtual)
+            {
+                case 1:
+                    pibSemaforo.Image = Properties.Resources.Verde; ;
+                    timer1.Interval = 5000; // 5 segundos para verde
+                    estadoAtual = 2;
+                    break;
+                case 2:
+                    pibSemaforo.Image = Properties.Resources.Amarelo; ;
+                    timer1.Interval = 2000; // 2 segundos para amarelo
+                    estadoAtual = 3;
+                    break;
+                case 3:
+                    pibSemaforo.Image = Properties.Resources.Vermelho;
+                    timer1.Interval = 5000; // 5 segundos para vermelho
+                    estadoAtual = 1;
+                    break;
+            }
+        }
+
+        private void btnParar_Click(object sender, EventArgs e)
+        {
+            // Serve para Apagar o Semáforo
+            pibSemaforo.Image = Properties.Resources.Apagado;
+            timer1.Stop();
+        }
+    }
+}
